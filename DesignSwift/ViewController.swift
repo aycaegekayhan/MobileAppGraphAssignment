7ec8757d05976a1dc1dd42d88e9aa226326ea544
Ayça ege Kayhan <aycaegekayhan@aycas-air.home>
2021-10-01 13:30:31 -0400

    Project started

---
+++ DesignSwift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DesignSwift
+//
+//  Created by Ayça ege Kayhan on 10/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonAction(_ sender: Any) {
+        
+    }
+    
+}
+
