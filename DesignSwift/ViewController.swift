916f96d1bde761fb712d3c935e7ff6694a710dee
Ayça ege Kayhan <aycaegekayhan@Aycas-MacBook-Air.local>
2021-10-03 15:32:52 -0400

    circle and animation button added

--- DesignSwift/ViewController.swift
+++ DesignSwift/ViewController.swift
@@ -8,16 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var myButton: UIButton!
+    
+    let shape = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func buttonAction(_ sender: Any) {
+        
+        let circlePath = UIBezierPath(arcCenter: view.center, radius: 150, startAngle: -(Double.pi/2), endAngle: (Double.pi) * 2, clockwise: true)
+        
+        shape.path = circlePath.cgPath
+        shape.lineWidth = 15
+        shape.strokeColor = UIColor.blue.cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        shape.strokeEnd = 0
+        view.layer.addSublayer(shape)
+        
+        let button = UIButton(frame: CGRect(x: 20, y: view.frame.size.width-250, width: view.frame.size.width-40, height: 50))
+        
+        view.addSubview(button)
+        button.setTitle("Animate", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         
     }
     
+    @objc func didTapButton() {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.toValue = 1
+        animation.duration = 3
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = .forwards
+        shape.add(animation, forKey: "animation")
+    }
+  
 }
 
